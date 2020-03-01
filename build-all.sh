975e1834f8296b8544c72eb2a692eb0c6c06cefe
htl-leonding <t.stuetz@htl-leonding.ac.at>
2020-03-01 21:24:33 +0100

    add automatic download of revealjs and build-all script

---
+++ build-all.sh
@@ -0,0 +1,4 @@
+set -e
+./build-html-docker.sh
+./build-reveal-docker.sh
+./build-pdf-docker.sh
---
+++ download-docker.sh
@@ -0,0 +1,6 @@
+set -e
+REVEAL_VERSION="3.9.2"
+curl -L https://github.com/hakimel/reveal.js/archive/$REVEAL_VERSION.zip --output revealjs.zip
+unzip revealjs.zip
+mv reveal.js-$REVEAL_VERSION ./docs/revealjs
+rm revealjs.zip
