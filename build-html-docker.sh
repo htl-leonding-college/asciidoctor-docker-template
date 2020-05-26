873ce1fc925516cff50d011bece9f55cf171afa2
htl-leonding <aenderemich12345>
2020-05-26 20:33:28 +0200

    change destination of gh-pages to gh-pages-branch

--- build-html-docker.sh
+++ build-html-docker.sh
@@ -3,14 +3,15 @@ set -e
 BUILD_DIR="docs"
 rm -rf -v $BUILD_DIR # else plantuml diagrams won't be rebuilt
 # do not copy revealjs
-mkdir -p docs
+mkdir -p $BUILD_DIR
 cp -r -p -v asciidocs/images $BUILD_DIR/images/
 cp -r -p -v asciidocs/themes $BUILD_DIR
 cp -r -p -v asciidocs/docinfo.html $BUILD_DIR
 cp -r -p -v asciidocs/.nojekyll $BUILD_DIR
 cp -r -p -v asciidocs/index.adoc $BUILD_DIR
 cp -r -p -v asciidocs/*.adoc $BUILD_DIR
-cp -r -p -v src $BUILD_DIR
+#uncomment it when you want to copy the source code into the gh-pages (for including source code into your document)
+#cp -r -p -v src $BUILD_DIR
 
 docker run --rm \
            -v ${PWD}/$BUILD_DIR:/documents \
@@ -30,7 +31,7 @@ docker run --rm \
            -a favicon=themes/favicon.png \
            -a sourcedir=src/main/java \
            -b html5 \
-           '*.adoc' && rm -rf ./.asciidoctor && echo Done"
+           '*.adoc' && rm -rf ./.asciidoctor && echo Creating html-docs in Docker finished ..."
 
 rm -rf -v $BUILD_DIR/revealjs
 #rm -rf -v $BUILD_DIR/.asciidoctor
--- publish-gh-pages.sh
+++ publish-gh-pages.sh
@@ -3,11 +3,13 @@
 # OR RATHER READ IT AND THEN USE
 # ONLY WORKS FROM THE PROJECT DIR
 # WILL PUBLISH STUFF ON THE WEBSITE, DON'T SCREW UP
+BUILD_DIR="gh-pages"
 export old=${PWD}
 ${PWD}/build-html-docker.sh
-cd docs
+cd $BUILD_DIR
+git init
 git add .
-git commit -m "update Demo Website"
-git push origin master
+git commit -m "$USER - rebuilding gh-pages $(date)"
+git remote add origin $(git config --get remote.origin.url)
+git push --force origin master:gh-pages
 cd $old
-
