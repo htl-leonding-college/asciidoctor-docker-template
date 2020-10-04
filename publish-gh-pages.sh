8d9851e3079a926603da7d257d2ef8671649b8df
htl-leonding <t.stuetz@htl-leonding.ac.at>
2020-10-04 14:32:54 +0200

    echo the url of the created gh-pages

--- publish-gh-pages.sh
+++ publish-gh-pages.sh
@@ -16,6 +16,22 @@ git push --force origin master:gh-pages
 cd $OLD
 rm -rf $BUILD_DIR
 
+# echo gh-pages url
+IFS='/' read -ra TEMP <<< "$ORIGIN_URL"
+#for i in "${TEMP[@]}"; do
+#    # process "$i"
+#    echo "$i"
+#done
+REPO_NAME=${TEMP[4]}
+echo
+echo "***********************************************************"
+echo created gh-pages under following url
+echo https://${TEMP[3]}.github.io/${REPO_NAME%.*}
+echo "***********************************************************"
+
+
 # Sources:
+# https://stackoverflow.com/a/965072
+# https://stackoverflow.com/a/918931
 # maybe better with subtrees -> https://gist.github.com/cobyism/4730490
 # https://blog.bloomca.me/2017/12/15/how-to-push-folder-to-github-pages.html
