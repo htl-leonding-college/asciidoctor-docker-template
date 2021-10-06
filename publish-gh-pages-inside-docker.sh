0de481cc01aad6a5120798258f740b348976f743
htl-leonding <t.stuetz@htl-leonding.ac.at>
2021-10-06 11:44:12 +0200

    fix error 'refname refs/heads/master not found'

--- publish-gh-pages-inside-docker.sh
+++ publish-gh-pages-inside-docker.sh
@@ -21,9 +21,20 @@ echo "GIT_GLOBAL_USER_NAME->${GIT_GLOBAL_USER_NAME}"
 git config --global user.email ${GIT_GLOBAL_MAIL}
 git config --global user.name ${GIT_GLOBAL_USER_NAME}
 git init
-git branch -M main
 git add .
 git commit -m "$GIT_GLOBAL_USER_NAME - rebuilding gh-pages $(date)"
+
+# rename branch to 'main' if necessary
+CURRENT_BRANCH=$(git branch --show-current)
+echo "current branch: $CURRENT_BRANCH"
+if [ "$CURRENT_BRANCH" != "main" ]
+then
+  git branch -M main
+  echo "branch $CURRENT_BRANCH renamed to 'main'"
+else
+  echo "branch $CURRENT_BRANCH not renamed"
+fi
+
 git remote add origin $ORIGIN_URL
 
 
